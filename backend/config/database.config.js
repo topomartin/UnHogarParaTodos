d328568731736b7af1be755595ac498814f78f74
Sladden S <Elizabeth.Sladden@hotmail.com>
2026-04-05 22:48:38 +0200

    Dejar de trackear DB config

--- backend/config/database.config.js
+++
@@ -1,9 +0,0 @@
-module.exports ={
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '1234',
-    database: 'un_hogar_para_todos',
-    synchronize: true,
-    logging:false
-}
